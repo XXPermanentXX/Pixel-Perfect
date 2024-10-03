4523a52c60e942fc044fa4517d03bfdf558dc180
Shiqi Gong <92624196+XXPermanentXX@users.noreply.github.com>
2024-10-03 13:31:03 +0800

    feat: replace the dependency of nextui

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,4 +1,4 @@
-import { NextUIProvider } from "@nextui-org/system";
+import { NextUIProvider } from "@nextui-org/react";
 import { configureStore } from "@reduxjs/toolkit";
 import { useNavigate } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
