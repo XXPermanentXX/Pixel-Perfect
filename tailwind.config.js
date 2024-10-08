b18620196d7dbd961c134684538a72f418695d8c
Shiqi Gong <92624196+XXPermanentXX@users.noreply.github.com>
2024-10-08 22:46:32 +0800

    feat: change the config of tailwing css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,49 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ["Open Sans", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"],
+      },
+      colors: {
+        "dark-bg": "#141619",
+        primary: "#4A46FF",
+      },
+      fontSize: {
+        headline: "4.875rem",
+        sectitle: "3.5rem",
+      },
+      buttonColor: {
+        primary: "#4A46FF",
+      },
+      keyframes: {
+        "slide-infinite": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        "scale-infinite": {
+          "0%, 100%": { transform: "scale(0.75)" },
+          "50%": { transform: "scale(1.05)" },
+        },
+      },
+      animation: {
+        "slide-infinite": "slide-infinite 30s linear infinite",
+        "scale-infinite": "scale-infinite 15s ease-in-out infinite",
+      },
+    },
   },
+
   darkMode: "class",
-  plugins: [nextui()],
-}
+
+  plugins: [
+    nextui({
+      defaultTheme: "dark",
+    }),
+  ],
+};
