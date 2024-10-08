ce84f4d13c1652da18b862da23b2d006399e5273
Shiqi Gong <92624196+XXPermanentXX@users.noreply.github.com>
2024-10-08 22:16:07 +0800

    feat: define some type and data

---
+++ src/models/staticDataModel.ts
@@ -0,0 +1,54 @@
+import { AspectRatio, Prompt, StyleItem } from "./types";
+import { realisticStyle, cartoonStyle, cinematicStyle } from "@/assets";
+
+const styleList: StyleItem[] = [
+  {
+    id: "realistic",
+    name: "Realistic",
+    thumbnail: realisticStyle,
+    keywords: "4k, hdr, realistic, high resolution, <lora:MG52:0.8>",
+  },
+  {
+    id: "cartoon",
+    name: "Cartoon",
+    thumbnail: cartoonStyle,
+    keywords: "(Pixar:1.3), 4k, disney, animation, whimsical, 3D, (disney pixar:1.3), high details, cartoon",
+  },
+  {
+    id: "cinematic",
+    name: "Cinematic",
+    thumbnail: cinematicStyle,
+    keywords: "good lighting, studio, (photoshoot:1.4), professional, 4k",
+  },
+];
+
+const aspectRatioList: AspectRatio[] = [
+  {
+    id: "1:1",
+    title: "1:1",
+  },
+  {
+    id: "4:3",
+    title: "4:3",
+  },
+  {
+    id: "16:9",
+    title: "16:9",
+  },
+];
+
+const initialPrompt: Prompt = {
+  prompt: "",
+  keywords: ["4k", " hdr", " realistic", " high resolution", " <lora:MG52:0.8>"],
+  aspectRatio: "",
+  type: "lora",
+  model: "",
+  triggerWord: "",
+  filename: "",
+  influence: 0.5,
+  generationSeed: "",
+  diffusionSteps: 20,
+  refine: true,
+};
+
+export { styleList, aspectRatioList, initialPrompt };
---
+++ src/models/types.ts
@@ -0,0 +1,35 @@
+export interface StyleItem {
+  id: string;
+  name: string;
+  thumbnail: string;
+  keywords: string;
+}
+
+export interface ModelItem {
+  id: string;
+  name: string;
+  thumbnail: string;
+  lora_model_name: string; 
+  trigger_word: string;
+}
+
+export interface AspectRatio {
+  id: string;
+  title: string;
+}
+
+export interface Prompt {
+  prompt: string;
+  keywords: string[];
+  aspectRatio: string;
+  type: string;
+  model: string;
+  triggerWord: string;
+  filename: string;
+  influence: number;
+  generationSeed: string;
+  diffusionSteps: number;
+  refine: boolean;
+}
+
+export type Status = "idle" | "loading" | "succeeded" | "failed";
