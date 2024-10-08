8e05a3f0a4b07b3eb439e693f85bdd2450f10514
Shiqi Gong <92624196+XXPermanentXX@users.noreply.github.com>
2024-10-08 22:15:49 +0800

    feat: image assets

---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+/**
+ * This file serves as a central repository for image and video assets used throughout the carousel component
+ * and potentially other parts of the web application. By exporting assets from a single location, it simplifies
+ * the process of importing and using these assets in various components, ensuring consistency and easing maintenance.
+ *
+ * The exports include:
+ * - Carousel images: A collection of images used specifically for the carousel component to display rotating banners or highlights.
+ * - Background blobs: Decorative background images to enhance the visual appeal of sections or components.
+ * - Feature images and videos: Assets used to highlight specific features, products, or services offered by the application.
+ * - Other assets: Including the founder's image for a personal touch in sections like 'About Us', and the company logo for branding purposes.
+ */
+
+// Style images
+export { default as cartoonStyle } from "./styles/cartoon.jpeg";
+export { default as cinematicStyle } from "./styles/cinematic.jpg";
+export { default as illustrationStyle } from "./styles/illustration.png";
+export { default as realisticStyle } from "./styles/realistic.jpg";
