40461379b2f75e0055235c6e55b40b9ecf24a81b
Changrong Li <631663510@qq.com>
2024-11-20 19:47:32 +0100

    feat: update view of login form

--- src/features/auth/LoginFormView.tsx
+++ src/features/auth/LoginFormView.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Input, Link, Checkbox } from "@nextui-org/react";
+import { Button, Input, Link, Checkbox, Spinner } from "@nextui-org/react";
 
 import { Status } from "@/models/types";
 import { UseFormFieldReturn } from "@/hooks/useFormField";
@@ -27,7 +27,6 @@ const LoginForm: React.FC<AuthViewProps> = ({
   setIsRememberMe,
   handleLogin,
   handleForgotPassword,
-
 }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [isForgotPassword, setIsForgotPassword] = useState(false);
@@ -41,161 +40,158 @@ const LoginForm: React.FC<AuthViewProps> = ({
   };
 
   return (
-    <div>
-      <header>
-        {isForgotPassword ? (
-          "🔒 Reset your password"
-        ) : (
-          <div className="flex flex-col">
+    <div className="mx-auto flex max-w-xl flex-col items-center">
+      {isForgotPassword ? (
+        "🔒 Reset your password"
+      ) : (
+        <div className="flex flex-col items-center space-y-5">
+          <h3 className="text-center font-semibold">
             ✨ Welcome to PixelPerfect AI ✨
-            <div className="pl-1 text-sm text-gray-500 font-light">
-              👉 Auto-registration on first login 👈
-            </div>
-          </div>
-        )}
-      </header>
+          </h3>
+          <p className="max-w-lg text-center">
+            👉 Auto-registration on first login 👈
+          </p>
+        </div>
+      )}
 
-      <main>
-        {isForgotPassword ? (
-          <>
-            <div className="h-[75px]">
-              <Input
-                isRequired
-                errorMessage={emailField.error}
-                isInvalid={emailField.isInvalid}
-                label="Email"
-                labelPlacement="outside"
-                placeholder="Enter your email"
-                description={
-                  RESET_MESSAGES[resetEmailState] || RESET_MESSAGES.default
-                }
-                value={emailField.value}
-                variant="bordered"
-                onChange={emailField.onChange}
-                onKeyDown={handleKeyDown}
-                classNames={{
-                  description:
-                    resetEmailState === "succeeded"
-                      ? "text-green-500"
-                      : "text-red-500",
-                }}
-              />
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="h-[75px]">
-              <Input
-                isRequired
-                errorMessage={emailField.error}
-                isInvalid={emailField.isInvalid}
-                label="Email"
-                labelPlacement="outside"
-                placeholder="Enter your email"
-                value={emailField.value}
-                variant="bordered"
-                onChange={emailField.onChange}
-                onKeyDown={handleKeyDown}
-              />
-            </div>
-            <div className="h-[75px]">
-              <Input
-                isRequired
-                endContent={
-                  <button
-                    className="focus:outline-none"
-                    type="button"
-                    onClick={() => setIsVisible(!isVisible)}
-                  >
-                    {isVisible ? (
-                      <EyeFilledIcon className="text-gray-400" />
-                    ) : (
-                      <EyeSlashFilledIcon className="text-gray-400" />
-                    )}
-                  </button>
-                }
-                errorMessage={
-                  authState === "failed"
-                    ? "Incorrect Password"
-                    : passwordField.error
-                }
-                isInvalid={authState === "failed" || passwordField.isInvalid}
-                label="Password"
-                labelPlacement="outside"
-                placeholder="Enter your password"
-                type={isVisible ? "text" : "password"}
-                value={passwordField.value}
-                variant="bordered"
-                onChange={passwordField.onChange}
-                onKeyDown={handleKeyDown}
-              />
-            </div>
-            <div className="flex justify-end pr-1 -mt-4">
-              <Link
-                className="text-cyan-500"
-                onClick={() => setIsForgotPassword(true)}
-                size="sm"
-              >
-                Forgot password?
-              </Link>
-            </div>
-            <div className="flex px-1 justify-center">
-              <Checkbox
-                isSelected={isRememberMe}
-                onValueChange={(value) => {
-                  setIsRememberMe(value);
-                }}
-                classNames={{
-                  label: "text-small",
-                }}
-              >
-                Remember me
-              </Checkbox>
-            </div>
-          </>
-        )}
-      </main>
-
-      <footer>
-        {isForgotPassword ? (
-          <div className="flex justify-center w-full gap-2">
-            <Button
-              color="default"
-              variant="flat"
-              onPress={() => setIsForgotPassword(false)}
-            >
-              Back
-            </Button>
-            <Button
-              color="primary"
-              onPress={handleForgotPassword}
-              isLoading={resetEmailState === "loading"}
-              isDisabled={emailField.isInvalid || emailField.value === ""}
-            >
-              Reset
-            </Button>
-          </div>
-        ) : (
-          <div className="flex justify-center w-full gap-2">
-            <Button color="default" variant="flat" as={Link} href="/">
-              Back
-            </Button>
-            <Button
-              color="primary"
-              isLoading={authState === "loading"}
-              isDisabled={
-                emailField.isInvalid ||
-                passwordField.isInvalid ||
-                emailField.value === "" ||
-                passwordField.value === ""
+      {isForgotPassword ? (
+        <>
+          <div className="h-[75px]">
+            <Input
+              isRequired
+              errorMessage={emailField.error}
+              isInvalid={emailField.isInvalid}
+              label="Email"
+              labelPlacement="outside"
+              placeholder="Enter your email"
+              description={
+                RESET_MESSAGES[resetEmailState] || RESET_MESSAGES.default
               }
-              onPress={handleLogin}
-            >
-              Log in
-            </Button>
+              value={emailField.value}
+              variant="bordered"
+              onChange={emailField.onChange}
+              onKeyDown={handleKeyDown}
+              classNames={{
+                description:
+                  resetEmailState === "succeeded"
+                    ? "text-green-500"
+                    : "text-red-500",
+              }}
+            />
           </div>
-        )}
-      </footer>
+        </>
+      ) : (
+        <div className="pb-10 pt-16 w-[400px]">
+          <div className="h-[75px]">
+            <Input
+              isRequired
+              errorMessage={emailField.error}
+              isInvalid={emailField.isInvalid}
+              label="Email"
+              labelPlacement="outside"
+              placeholder="Enter your email"
+              value={emailField.value}
+              variant="bordered"
+              onChange={emailField.onChange}
+              onKeyDown={handleKeyDown}
+            />
+          </div>
+          <div className="h-[75px]">
+            <Input
+              isRequired
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => setIsVisible(!isVisible)}
+                >
+                  {isVisible ? (
+                    <EyeFilledIcon className="text-gray-400" />
+                  ) : (
+                    <EyeSlashFilledIcon className="text-gray-400" />
+                  )}
+                </button>
+              }
+              errorMessage={
+                authState === "failed"
+                  ? "Incorrect Password"
+                  : passwordField.error
+              }
+              isInvalid={authState === "failed" || passwordField.isInvalid}
+              label="Password"
+              labelPlacement="outside"
+              placeholder="Enter your password"
+              type={isVisible ? "text" : "password"}
+              value={passwordField.value}
+              variant="bordered"
+              onChange={passwordField.onChange}
+              onKeyDown={handleKeyDown}
+            />
+          </div>
+          <div className="flex justify-end pr-1 -mt-4">
+            <Link
+              className="text-cyan-500"
+              onClick={() => setIsForgotPassword(true)}
+              size="sm"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          <div className="flex pt-2 px-1 justify-center">
+            <Checkbox
+              isSelected={isRememberMe}
+              onValueChange={(value) => {
+                setIsRememberMe(value);
+              }}
+              classNames={{
+                label: "text-small",
+              }}
+            >
+              Remember me
+            </Checkbox>
+          </div>
+        </div>
+      )}
+
+      {isForgotPassword ? (
+        <div className="flex justify-center w-full gap-2">
+          <Button
+            color="default"
+            variant="flat"
+            onPress={() => setIsForgotPassword(false)}
+          >
+            Back
+          </Button>
+          <Button
+            color="primary"
+            onPress={handleForgotPassword}
+            isLoading={resetEmailState === "loading"}
+            isDisabled={emailField.isInvalid || emailField.value === ""}
+          >
+            Reset
+          </Button>
+        </div>
+      ) : (
+        <div className="flex justify-center space-x-10">
+          <Button  size="lg" radius="full" variant="solid" color="primary" className="w-48 bg-white text-primary" as={Link} href="/">
+            Back
+          </Button>
+          <Button
+            size="lg" radius="full" variant="solid" color="primary" className="w-48"
+            isLoading={authState === "loading"}
+            isDisabled={
+              emailField.isInvalid ||
+              passwordField.isInvalid ||
+              emailField.value === "" ||
+              passwordField.value === ""
+            }
+            spinner={<Spinner color="white" size="sm" />}
+            onPress={handleLogin}
+          >
+            Log in
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
