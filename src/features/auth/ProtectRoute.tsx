224d8f61d8f37e67a419ec305f98376a970989be
Changrong Li <lithium0813@live.com>
2024-11-24 00:06:53 +0800

    feat:ProtectRoute

--- src/features/auth/ProtectRoute.tsx
+++ src/features/auth/ProtectRoute.tsx
@@ -1,10 +1,19 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState} from "@/provider";
+import { useEffect, useState } from "react";
+import { firebaseConfig } from "@/models/apiConfig";
 
 export default function ProtectRoute({children}: {children: React.ReactNode }){
-  const {  user } = useSelector((state: RootState) => state.auth);
-  if (!user) {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  
+  useEffect(() => {
+    const userInfo = localStorage.getItem('firebase:authUser:'+firebaseConfig.apiKey+':[DEFAULT]')
+    setIsAuthenticated(!!userInfo)
+  }, [user?.userId]);
+
+  if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
 
